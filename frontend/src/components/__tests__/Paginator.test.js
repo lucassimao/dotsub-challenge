c627d7abb2074d258b09b177e0bc3b88f88a77a6
Lucas Simão <lucas.simao@trt22.jus.br>
2019-07-29 16:42:42 -0300

    TDD Paginator

---
+++ frontend/src/components/__tests__/Paginator.test.js
@@ -0,0 +1,124 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
+import App, { DEFAULT_PAGE_SIZE } from "../App";
+
+import FileService, { mockFileServiceList } from "../../services/FileService";
+
+jest.mock("../../services/FileService");
+
+let container, modalRoot;
+
+beforeEach(() => {
+  jest.useFakeTimers();
+});
+
+beforeEach(() => {
+  container = document.createElement("div");
+  modalRoot = document.createElement("div");
+  modalRoot.setAttribute("id", "modal-root");
+
+  document.body.appendChild(container);
+  document.body.appendChild(modalRoot);
+
+  FileService.mockClear();
+  mockFileServiceList.mockClear();
+});
+
+afterEach(() => {
+  ReactDOM.unmountComponentAtNode(container);
+
+  document.body.removeChild(modalRoot);
+  document.body.removeChild(container);
+
+  container = null;
+  modalRoot = null;
+});
+
+it("should show buttons appropriately", async () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+
+  await act(async () => {
+    jest.runAllTimers();
+  });
+
+  // 1 button for the 10 pages and the 'Next' button
+  let paginatorButtons = document.querySelectorAll(".Paginator .buttons button");
+  expect(paginatorButtons.length).toBe(11);
+
+  // buton '1' must be active
+  let selectedPaginatorButton = container.querySelector(".Paginator .buttons .active");
+  expect(selectedPaginatorButton.textContent).toBe("1");
+
+  // last button must be the 'Next' button
+  let lastButton = container.querySelector(".Paginator .buttons :last-child");
+  expect(lastButton.textContent).toBe("Next");
+
+  act(() => {
+    lastButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  // when the 'Next' button is clicked for the very fist time, there must be 12 buttons
+  // because the 'Previous' button is show
+  paginatorButtons = document.querySelectorAll(".Paginator .buttons button");
+  expect(paginatorButtons.length).toBe(12); // 'Previous' button, 10 buttons for each page, 'Next' button
+
+  // ... and the button '2' is the active one
+  selectedPaginatorButton = container.querySelector(".Paginator .buttons .active");
+  expect(selectedPaginatorButton.textContent).toBe("2");
+
+  // given the button '10', when clicked, the 'next' button must hide, once there is not next pages
+  let _10thPageButton = document.querySelector(".Paginator .buttons :nth-last-child(2)");
+  expect(_10thPageButton.textContent).toBe("10");
+
+  act(() => {
+    _10thPageButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  selectedPaginatorButton = container.querySelector(".Paginator .buttons .active");
+  expect(selectedPaginatorButton.textContent).toBe("10");
+
+  lastButton = container.querySelector(".Paginator .buttons :last-child");
+  expect(lastButton.textContent).toBe("10");
+});
+
+it("FileService should request new pages to the backend when buttons are clicked", async () => {
+    
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+
+  await act(async () => {
+    jest.runAllTimers();
+  });
+
+
+  let nextButton = container.querySelector(".Paginator .buttons :last-child");
+  expect(nextButton.textContent).toBe("Next");
+
+  act(() => {
+    nextButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  await act(async () => {
+    jest.runAllTimers();
+  });
+
+
+  act(() => {
+    nextButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  await act(async () => {
+    jest.runAllTimers();
+  });
+
+  // the 2 click on the button next + on first render of the component
+  expect(mockFileServiceList.mock.calls.length).toBe(3);
+  expect(mockFileServiceList.mock.calls[0]).toEqual([0,DEFAULT_PAGE_SIZE]);
+  expect(mockFileServiceList.mock.calls[1]).toEqual([1,DEFAULT_PAGE_SIZE]);
+  expect(mockFileServiceList.mock.calls[2]).toEqual([2,DEFAULT_PAGE_SIZE]);
+  
+});
