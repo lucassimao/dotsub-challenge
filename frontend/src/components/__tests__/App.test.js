f8c6be54fc74800d3ff8072ce631bcc8134f136b
Lucas Simão <lucas.simao@trt22.jus.br>
2019-07-29 15:54:30 -0300

    TDD App component

--- frontend/src/components/__tests__/App.test.js
+++ frontend/src/components/__tests__/App.test.js
@@ -1,9 +1,112 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
+import App, { DEFAULT_PAGE_SIZE } from "../App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import FileService, { mockFileServiceList } from "../../services/FileService";
+
+jest.mock("../../services/FileService");
+
+let container, modalRoot;
+
+beforeEach(() => {
+  jest.useFakeTimers();
+});
+
+beforeEach(() => {
+  container = document.createElement("div");
+  modalRoot = document.createElement("div");
+  modalRoot.setAttribute("id", "modal-root");
+
+  document.body.appendChild(container);
+  document.body.appendChild(modalRoot);
+
+  FileService.mockClear();
+  mockFileServiceList.mockClear();
+});
+afterEach(() => {
+  ReactDOM.unmountComponentAtNode(container);
+
+  document.body.removeChild(modalRoot);
+  document.body.removeChild(container);
+
+  container = null;
+  modalRoot = null;
+});
+
+it("renders without crashing", () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+
+  const searchBox = container.getElementsByClassName("SearchBox")[0];
+  expect(searchBox).not.toBeUndefined();
+
+  const table = container.getElementsByTagName("table")[0];
+  expect(table).not.toBeUndefined();
+
+  const paginator = container.getElementsByClassName("Paginator")[0];
+  expect(paginator).not.toBeUndefined();
+});
+
+it("loads the initial page of data, showing in the table and configures the paginator", async () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+
+  await act(async () => {
+    jest.runAllTimers();
+  });
+
+  expect(mockFileServiceList).toHaveBeenCalledWith(0, DEFAULT_PAGE_SIZE);
+
+  const tableRows = container.querySelectorAll("table tbody tr");
+  // table must have as much rows as the default paging size
+  expect(tableRows.length).toBe(DEFAULT_PAGE_SIZE);
+
+  const paginatorPageSizeInfo = container.querySelector("#page-size");
+  expect(paginatorPageSizeInfo.textContent).toBe("10");
+
+  const paginatorTotalEntriesInfo = container.querySelector("#total-entries");
+  expect(paginatorTotalEntriesInfo.textContent).toBe("100");
+
+  const selectedPaginatorButton = container.querySelector(
+    ".Paginator .buttons .active"
+  );
+  expect(selectedPaginatorButton.textContent).toBe("1");
+});
+
+it("loads the appropriate page of data after clicking the paginator's buttons", async () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+
+  await act(async () => {
+    jest.runAllTimers();
+  });
+
+  const buttonNext = container.querySelector(".Paginator .buttons :last-child");
+  expect(buttonNext.textContent).toBe("Next");
+
+  act(() => {
+    buttonNext.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  let selectedPaginatorButton = container.querySelector(
+    ".Paginator .buttons .active"
+  );
+  expect(selectedPaginatorButton.textContent).toBe("2");
+
+  // clicking on button 9 loads the 9th page
+  const button9 = container.querySelector(".Paginator .buttons :nth-child(10)");
+  expect(button9.textContent).toBe("9");
+
+  act(() => {
+    button9.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  selectedPaginatorButton = container.querySelector(
+    ".Paginator .buttons .active"
+  );
+  expect(selectedPaginatorButton.textContent).toBe("9");
 });
