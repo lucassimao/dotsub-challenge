0fd8b336255b9005a061933d35f0392d7c3758ef
Lucas Simao <Lucas Simao>
2019-07-30 01:04:18 -0300

    removing unused imports

--- backend/src/main/java/com/dotsub/challenge/config/WebSecurityConfig.java
+++ backend/src/main/java/com/dotsub/challenge/config/WebSecurityConfig.java
@@ -2,11 +2,8 @@ package com.dotsub.challenge.config;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.Profiles;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
